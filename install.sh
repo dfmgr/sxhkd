e3b7feacb8ff3bbcd247be7eb30d7cd56534a269
Jason <jason@casjay.net>
2021-01-19 09:12:47 -0500

    🏠🐜❗ Minor Fixes ❗🐜🏠

--- install.sh
+++ install.sh
@@ -155,6 +155,7 @@ failexitcode
 
 # Plugins
 
+if __am_i_online; then
 if [ "$PLUGNAMES" != "" ]; then
   if [ -d "$PLUGDIR"/PLUREP/.git ]; then
     execute \
@@ -165,6 +166,7 @@ if [ "$PLUGNAMES" != "" ]; then
       "git_clone PLUGINREPO $PLUGDIR/PLUGREP" \
       "Installing plugin PLUGREP"
   fi
+fi
 fi
 
 # exit on fail
